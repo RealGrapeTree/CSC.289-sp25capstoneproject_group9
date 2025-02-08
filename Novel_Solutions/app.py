7fedc829e2936bf298a68a4ac44e4ca976a003c3
jakemlock <145785049+jakemlock@users.noreply.github.com>
2025-02-08 17:17:22 -0500

    Added extensions.py started setting up database

--- Novel_Solutions/app.py
+++ Novel_Solutions/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from dotenv import load_dotenv
 from blueprints.loginpage.Novel_login import Novel_login
+from .extensions import db
 
 # import os module to access environment variables
 import os
@@ -12,8 +13,17 @@ load_dotenv()
 # Create an instance of the Flask application
 app = Flask(__name__)
 
+# Set the secret key to the SECRET_KEY environment variable
 app.secret_key = os.getenv('SECRET_KEY')
 
+# Configure the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db.init_app(app)
+
+
+
+
+# registers the Novel login blueprint to the app 
 app.register_blueprint(Novel_login)
 
 
