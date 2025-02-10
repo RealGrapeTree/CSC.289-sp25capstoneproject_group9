0be6ffca4e88492c8fb1970a92482f9bc7c60beb
Sally-ulani <81251778+Sally-ulani@users.noreply.github.com>
2025-02-10 18:59:31 -0500

    Update book_search.py

--- book_search.py
+++ book_search.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     isbn = db.Column(db.String(20), unique=True, nullable=False)
-    sku = db.Column(db.String(20), unique=True, nullable=False)
+    sku = db.Column(db.String(20), unique=True, nullable=True)
     title = db.Column(db.String(100), nullable=False)
     stock = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
